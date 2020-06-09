1ff99aae4d14b589ed44022db7a377768b43c568
guigomes94 <guigomes94@outlook.com>
2020-06-09 07:52:14 -0300

    rotas e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-export default function App() {
-  return <h1>Hello World</h1>;
-}
+import GlobalStyle from "./styles/global";
+import Routes from "./routes/index";
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
+
+export default App;
