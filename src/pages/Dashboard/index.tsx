729a372b70386e495f3196dc30adb7b71f69dc8c
guigomes94 <guigomes94@outlook.com>
2020-06-10 07:01:48 -0300

    estilazando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import logo from "../../assets/logo.svg";
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://img.quizur.com/f/img5c884840a62cc9.48055575.jpg?lastEdited=1552435268"
+            alt="avatar"
+          />
+          <div>
+            <strong>Dragon Ball API</strong>
+            <p>Api com personagens do universo Dragon Ball</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
