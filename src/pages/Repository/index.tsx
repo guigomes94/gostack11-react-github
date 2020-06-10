71554ba2aa7431e0ec3b53f0d3932d87e7e0525f
guigomes94 <guigomes94@outlook.com>
2020-06-10 09:30:16 -0300

    estilazando detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import logo from "../../assets/logo.svg";
+import { Header, RepositoryInfo, Issues } from "./styles";
+import api from "../../services/api";
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatarfiles.alphacoders.com/782/78200.png"
+            alt="asdasd"
+          />
+          <div>
+            <strong>skdlanksdnlasd</strong>
+            <p>adknaklsndkanskda</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>2000</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>2000</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>2000</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="{`/repository/${repository.full_name}`}">
+          <div>
+            <strong>sdasdasda</strong>
+            <p>asdasda</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
